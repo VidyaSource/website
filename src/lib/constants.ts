2788fe93f015d560adae75681ce3a24ee38bd9df
neilchaudhuri <neil@vidyasource.com>
2021-05-01 19:18:27 -0400

    Constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,19 @@
+export interface Constants {
+    people: {
+        president: {
+            name: string
+            linkedIn: string
+        }
+    }
+}
+export const constants: Constants = {
+    people: {
+        president: {
+            name: "Neil Chaudhuri",
+            linkedIn: "https://www.linkedin.com/in/neil-chaudhuri/"
+        }
+    }
+}
+
+
+
