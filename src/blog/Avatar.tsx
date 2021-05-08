11061578e899f4c2d1ab5ba042ad9cadd0fab7e0
neilchaudhuri <neil@vidyasource.com>
2021-05-08 16:35:38 -0400

    Simplify API by using constants

--- src/blog/Avatar.tsx
+++ src/blog/Avatar.tsx
@@ -2,15 +2,11 @@ import Image from 'next/image'
 import {constants} from "../lib/constants";
 
 interface AvatarProps {
-    author?: string
-    image: string
-    alt: string
-    profileUrl?: string
+    author: string
 }
 
 export const Avatar = (p: AvatarProps) => {
-    const author = p.author ?? constants.people.president.name
-    const profileUrl = p.profileUrl ?? constants.people.president.twitter
+    const {profileUrl, image } = constants[p.author]
     return (
         <a href={profileUrl} className="flex-shrink-0 group block">
             <div className="flex items-center justify-center">
@@ -19,11 +15,11 @@ export const Avatar = (p: AvatarProps) => {
                     width="60"
                     quality={100}
                     className="inline-block h-9 w-9 rounded-full"
-                    src={p.image}
-                    alt={p.alt}
+                    src={image}
+                    alt={p.author}
                 />
                 <span className="ml-3">
-                    <p className="pb-0 mb-0 text-sm font-medium text-red group-hover:text-blue lg:text-md">{author}</p>
+                    <p className="pb-0 mb-0 text-sm font-medium text-red group-hover:text-blue lg:text-md">{p.author}</p>
                     <p className="pt-0 mt-0 text-xs font-medium text-red group-hover:text-blue lg:text-xs">View profile</p>
                 </span>
             </div>
