4867741e1313d646c732f1b78e74ee1992c4845a
neilchaudhuri <neil@vidyasource.com>
2021-05-08 20:06:08 -0400

    Remove React Query

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,33 +1,25 @@
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
 import {Page} from "../../components/Page";
 import {BlogPostHeadElement} from "../../blog/BlogPostHeadElement";
 import {BlogPost, getAllBlogPosts, getBlogPostBySlug} from "../../lib/blogPost-utils";
 import {BlogPostContent} from "../../blog/BlogPostContent";
-import {useQuery} from "react-query";
 
-interface PostProps {
-    blogPost: BlogPost
-}
-
-const Post = (p: PostProps) => {
+const Post = (blogPost: BlogPost) => {
     const router = useRouter()
-    const {data} = useQuery(['post', p.blogPost.slug], () => getBlogPostBySlug(p.blogPost.slug))
-    if (!router.isFallback && !data.slug) {
-        return <ErrorPage statusCode={404}/>
+    if (!router.isFallback && !blogPost.slug) {
+        return <ErrorPage statusCode={404} />
     }
-
     return (
-        <Page headElement={<BlogPostHeadElement title={data.frontMatter.title}/>}>
-            <BlogPostContent blogPost={data}/>
+        <Page headElement={<BlogPostHeadElement title={blogPost.frontMatter.title}/>}>
+            <BlogPostContent blogPost={blogPost} />
         </Page>
-
     )
 }
 
 export default Post
 
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
     const post = await getBlogPostBySlug(params.slug)
 
     return {
