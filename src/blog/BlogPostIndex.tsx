2d81fb847b747426b3b41004be179c6d03ae61e4
neilchaudhuri <neil@vidyasource.com>
2021-05-09 19:16:16 -0400

    Hooks for tag filter

--- src/blog/BlogPostIndex.tsx
+++ src/blog/BlogPostIndex.tsx
@@ -1,6 +1,7 @@
 import {BlogPost, getBlogPostsByTags} from "../lib/blogPost-utils";
 import {BlogPostCard} from "./BlogPostCard";
 import {useQuery} from "react-query";
+import {ChangeEvent, useEffect, useState} from "react";
 
 interface BlogPostIndexProps {
     blogPosts: BlogPost[]
@@ -9,15 +10,33 @@ interface BlogPostIndexProps {
 export const BlogPostIndex = (p: BlogPostIndexProps) => {
     const {data} = useQuery('postsByTags', getBlogPostsByTags)
     const tags = Object.keys(data).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+    const [selectedTag, selectTag] = useState<string>("")
+    const [currentPosts, setCurrentPosts] = useState<BlogPost[]>(p.blogPosts)
+    useEffect(() => {
+        if (selectedTag === "") {
+            setCurrentPosts(p.blogPosts)
+        } else {
+            setCurrentPosts(data[selectedTag])
+        }
+    }, [p.blogPosts, selectedTag])
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        const tag = e.target.value
+        if (tag) {
+            selectTag(tag)
+        } else {
+            selectTag("")
+        }
+    }
 
     return (
         <div className="relative bg-gray-50 pt-8 pb-20 px-4 sm:px-6 lg:pt-16 lg:pb-28 lg:px-8">
             <div className="flex flex-col">
                 <label htmlFor="tag" className="mx-auto block text-lg font-medium text-red md:text-xl">
-                    Search by tag
+                    Filter by tag
                 </label>
                 <div className="mt-1">
-                    <select id="tag" name="tag" placeholder="Search..."
+                    <select id="tag" name="tag" placeholder="Search..." onChange={onChange}
                             className="mx-auto block text-red sm:text-xl lg:text-2xl focus:border-green-dark md:w-full lg:w-full shadow-sm sm:max-w-xs sm:text-sm border-red rounded-md">
                         <option value=""/>
                         {
@@ -29,7 +48,7 @@ export const BlogPostIndex = (p: BlogPostIndexProps) => {
             <div className="relative max-w-7xl mx-auto">
                 <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
                     {
-                        p.blogPosts.map((blogPost) => (
+                        currentPosts.map((blogPost) => (
                             <BlogPostCard key={blogPost.slug} blogPost={blogPost}/>
                         ))
                     }
