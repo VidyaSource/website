4d533b557003a5858fa448e9f286fb23f5fe46fe
neilchaudhuri <neil@vidyasource.com>
2021-05-08 20:43:29 -0400

    Improve arrangement

--- src/blog/BlogPostIndex.tsx
+++ src/blog/BlogPostIndex.tsx
@@ -7,12 +7,16 @@ interface BlogPostIndexProps {
 
 export const BlogPostIndex = (p: BlogPostIndexProps) => {
     return (
-        <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            {p.blogPosts.map((blogPost) => (
-                <li key={blogPost.slug} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
-                    <BlogPostCard blogPost={blogPost} />
-                </li>
-            ))}
-        </ul>
+        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+            <div className="relative max-w-7xl mx-auto">
+                <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+                    {
+                        p.blogPosts.map((blogPost) => (
+                            <BlogPostCard key={blogPost.slug} blogPost={blogPost}/>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
