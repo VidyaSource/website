2e4542e8c60c7ae68370478a37b2761b33b1014d
neilchaudhuri <neil@vidyasource.com>
2021-05-12 17:19:03 -0400

    Screenreader element

--- src/blog/Avatar.tsx
+++ src/blog/Avatar.tsx
@@ -23,6 +23,7 @@ export const Avatar = (p: AvatarProps) => {
                     <span className="ml-3">
                     <p className="pb-0 mb-0 text-sm font-medium text-red group-hover:text-blue lg:text-md">{p.author}</p>
                     <p className="pt-0 mt-0 text-xs font-medium text-red group-hover:text-blue lg:text-xs">
+                        <span className="sr-only">Twitter</span>
                         <svg fill="#1DA1F2" viewBox="0 0 24 24" className="h-6 w-6" aria-hidden="true">
                             <path
                                 d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"/>
