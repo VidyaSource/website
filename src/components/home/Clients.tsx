1fb481b3d55a031899c669ccba05a2995d3b68ff
neilchaudhuri <neil@vidyasource.com>
2021-05-14 18:46:39 -0400

    Initial customization

--- src/components/home/Clients.tsx
+++ src/components/home/Clients.tsx
@@ -1,30 +1,22 @@
 /* This example requires Tailwind CSS v2.0+ */
 export const Clients = () => {
     return (
-        <div className="bg-white">
+        <div className="bg-green-light">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
                 <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
                     <div>
-                        <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-                            Used by the world's most average companies
+                        <h2 className="text-3xl font-extrabold text-red sm:text-4xl">
+                            Government and business trust Vidya.
                         </h2>
-                        <p className="mt-3 max-w-3xl text-lg text-gray-500">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a
-                            scelerisque amet ullamcorper eu enim et fermentum, augue.
+                        <p className="mt-3 max-w-3xl text-lg text-gray-dark">
+                            We are proud to have brought our expertise and insight to large government agencies, small businesses, and
+                            everything in between to help them all capitalize on an ever-evolving technology landscape.
                         </p>
                         <div className="mt-8 sm:flex">
                             <div className="rounded-md shadow">
                                 <a
                                     href="#"
-                                    className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-                                >
-                                    Create Account
-                                </a>
-                            </div>
-                            <div className="mt-3 sm:mt-0 sm:ml-3">
-                                <a
-                                    href="#"
-                                    className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
+                                    className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red hover:bg-indigo-700"
                                 >
                                     Contact Us
                                 </a>
@@ -32,37 +24,37 @@ export const Clients = () => {
                         </div>
                     </div>
                     <div className="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
-                        <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+                        <div className="col-span-1 flex justify-center py-8 px-8 bg-blue-light">
                             <img
-                                className="max-h-12"
-                                src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+                                className="max-h-36"
+                                src="img/partners/dos.svg"
                                 alt="Workcation"
                             />
                         </div>
-                        <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                            <img className="max-h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage" />
-                        </div>
-                        <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                            <img className="max-h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" />
-                        </div>
-                        <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+                        <div className="col-span-1 flex justify-center py-8 px-8 bg-blue-light">
                             <img
-                                className="max-h-12"
-                                src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg"
-                                alt="Laravel"
-                            />
-                        </div>
-                        <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                            <img
-                                className="max-h-12"
-                                src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+                                className="max-h-36"
+                                src="img/partners/trss.png"
                                 alt="StaticKit"
                             />
                         </div>
-                        <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+                        <div className="col-span-1 flex justify-center py-8 px-8 bg-blue-light">
+                            <img className="max-h-36" src="img/partners/healthcaregov.png" alt="Tuple" />
+                        </div>
+                        <div className="col-span-1 flex justify-center py-8 px-8 bg-blue-light">
                             <img
-                                className="max-h-12"
-                                src="https://tailwindui.com/img/logos/statamic-logo-gray-400.svg"
+                                className="max-h-36"
+                                src="img/partners/ninaday.svg"
+                                alt="Laravel"
+                            />
+                        </div>
+                        <div className="col-span-1 flex justify-center py-8 px-8 bg-blue-light">
+                            <img className="max-h-36" src="img/partners/eop.png" alt="Mirage" />
+                        </div>
+                        <div className="col-span-1 flex justify-center py-8 px-8 bg-blue-light">
+                            <img
+                                className="max-h-36"
+                                src="img/partners/gcn.png"
                                 alt="Statamic"
                             />
                         </div>
