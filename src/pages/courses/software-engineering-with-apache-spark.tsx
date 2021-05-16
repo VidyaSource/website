a44cf37232131540cf20c74c890b3baadc30b6f4
neilchaudhuri <neil@vidyasource.com>
2021-05-16 17:37:47 -0400

    Initial Instructor

--- src/pages/courses/software-engineering-with-apache-spark.tsx
+++ src/pages/courses/software-engineering-with-apache-spark.tsx
@@ -8,6 +8,7 @@ export const Spark = () => {
         <Page headElement={<CourseHeadElement title="Vidya | Software Engineering with Apache Spark"  />}>
             <CourseHeader />
             <Description />
+            <Instructor />
         </Page>
     )
 }
@@ -99,6 +100,64 @@ const Description = () => {
         </div>
     )
 }
+
+const Instructor = () => {
+    return (
+        <section className="bg-red-light overflow-hidden">
+            <div className="relative max-w-7xl mx-auto pt-20 pb-12 px-4 sm:px-6 lg:px-8 lg:py-20">
+
+                <div className="relative lg:flex lg:items-center">
+                    <div className="hidden lg:block lg:flex-shrink-0">
+                        <img
+                            className="h-64 w-64 rounded-full xl:h-80 xl:w-80"
+                            src="/img/staff/president.png"
+                            alt=""
+                        />
+                    </div>
+
+                    <div className="relative lg:ml-10">
+                        <svg
+                            className="absolute top-0 left-0 transform -translate-x-8 -translate-y-24 h-36 w-36 text-indigo-200 opacity-50"
+                            stroke="#455930"
+                            fill="none"
+                            viewBox="0 0 144 144"
+                            aria-hidden="true">
+                            <path
+                                strokeWidth={2}
+                                d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z"
+                            />
+                        </svg>
+                        <blockquote className="relative">
+                            <div className="text-2xl leading-9 font-medium text-gray-900">
+                                <p>
+                                    I have built several Scala and Apache Spark applications currently in production, and I
+                                    worked with the original Apache Spark team, AMPLab at UC Berkeley, on a research project
+                                    for DARPA known as XDATA. I have Spark and Scala badges on Stack Overflow. has extensive knowledge of Spark and believes very strongly in it.
+                                </p>
+                            </div>
+                            <footer className="mt-8">
+                                <div className="flex">
+                                    <div className="flex-shrink-0 lg:hidden">
+                                        <img
+                                            className="h-12 w-12 rounded-full"
+                                            src="/img/staff/president.png"
+                                            alt=""
+                                        />
+                                    </div>
+                                    <div className="ml-4 lg:ml-0">
+                                        <div className="text-lg lg:text-xl font-medium text-gray-dark">Neil Chaudhuri</div>
+                                        <div className="text-base lg:text-lg font-medium text-red">Course instructor</div>
+                                    </div>
+                                </div>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
 
 
 
