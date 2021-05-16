861b8d6f08c4c241b93e3512325afbe89534b9d7
neilchaudhuri <neil@vidyasource.com>
2021-05-16 14:53:59 -0400

    Aspect sizing; update text; opacity

--- src/pages/courses/software-engineering-with-apache-spark.tsx
+++ src/pages/courses/software-engineering-with-apache-spark.tsx
@@ -36,30 +36,47 @@ const Description = () => {
         <div className="relative bg-white">
             <div className="lg:absolute lg:inset-0">
                 <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
-                        <Image src="/img/courses/img.png" height={1000} width={667} quality={100}  />
+                    <div className="aspect-h-9 aspect-w-16">
+                        <Image
+                            src="/img/courses/img.png"
+                            alt="Collaborating software engineers"
+                            layout="fill"
+                            objectFit="cover"
+                            quality={100}
+                            className="opacity-10 lg:opacity-100"
+                        />
+                    </div>
                 </div>
             </div>
             <div className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
                 <div className="lg:col-start-2 lg:pl-8">
                     <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
                         <h2 className="leading-6 text-red font-semibold tracking-wide uppercase">Description</h2>
-                        <p className="mt-8 text-lg text-gray-dark">
-                            Apache Spark has become a popular alternative for companies who want their analytics
+                        <p className="mt-8 text-lg font-medium lg:text-gray-dark lg:font-normal">
+                            Apache Spark is great for organizations who want their analytics
                             developed fast and executed fast. The problem is that large-scale distributed computing is
                             always hard. It’s not like you can set a breakpoint on code running on multiple machines when
                             things go wrong. Monitoring your analytics jobs and optimizing accordingly are important. You
                             also have to rethink the way you approach concepts like architecture, security, and software
                             engineering techniques like testing and DevSecOps.
                         </p>
+                        <p className="mt-8 text-lg font-medium lg:text-gray-dark lg:font-normal">
+                            <span className=" text-red font-semibold">Software Engineering with Apache Spark</span> teaches
+                            you how to leverage just enough Scala to integrate
+                            powerful data pipelines into your existing monolithic, microservices, and/or serverless architecture.
+                            You will also learn how your pipelines can benefit from the same testing, continuous integration,
+                            containerization with Docker, container orchestration with Kubernetes, and security hardening
+                            as the others components of your enterprise.
+                        </p>
                         <div className="mt-5 text-gray-dark">
-                            <h3>What makes this course different</h3>
-                            <p>
+                            <h3 className="font-semibold">What makes this course different</h3>
+                            <p className="font-medium lg:text-gray-dark lg:font-normal">
                                 Apache Spark is a huge topic, and the typical Spark course tries to cover too much in too
                                 short a period, which leaves you overwhelmed. Meanwhile, it ignores any discussion of
                                 architectural patterns, security, and software engineering--the things that separate experiments
                                 in your garage from critical pieces of your production architecture.
                             </p>
-                            <p>
+                            <p className="font-medium">
                                 We take a more practical approach. There are rich code examples providing deep insight
                                 into the API as you’d expect, but the exercises utilize real datasets from Data.gov and
                                 encourage you to collaborate with your peers, the Spark Scaladoc, Stack Overflow, and
@@ -71,8 +88,7 @@ const Description = () => {
                                 In addition, agile software development and DevOps have taught us the value of build
                                 automation, testing, continuous integration, and continuous delivery, and we don’t have
                                 to abandon them just because we are working in a distributed environment. In fact, the
-                                complexity of distributed systems makes them even more valuable. Software Engineering
-                                with Apache Spark shows you how to apply these techniques to improve the quality and
+                                complexity of distributed systems makes them even more valuable. <span className=" text-red font-semibold">Software Engineering with Apache Spark</span> shows you how to apply these techniques to improve the quality and
                                 reliability of your analytics.
                             </p>
                         </div>
