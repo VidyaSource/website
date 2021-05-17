76dccd04c45694b3a85a0cfe085986870c8273f7
neilchaudhuri <neil@vidyasource.com>
2021-05-16 21:21:58 -0400

    Use Image

--- src/pages/courses/software-engineering-with-apache-spark.tsx
+++ src/pages/courses/software-engineering-with-apache-spark.tsx
@@ -5,12 +5,12 @@ import {CallToAction} from "../../components/home/CallToAction";
 
 export const Spark = () => {
     return (
-        <Page headElement={<CourseHeadElement title="Vidya | Software Engineering with Apache Spark"  />}>
-            <CourseHeader />
-            <Description />
-            <Syllabus />
-            <Instructor />
-            <CallToAction />
+        <Page headElement={<CourseHeadElement title="Vidya | Software Engineering with Apache Spark"/>}>
+            <CourseHeader/>
+            <Description/>
+            <Syllabus/>
+            <Instructor/>
+            <CallToAction/>
         </Page>
     )
 }
@@ -25,7 +25,8 @@ export const CourseHeader = () => {
                         Software Engineering with Apache Spark
                     </h2>
                     <p className="max-w-xl mt-5 mx-auto text-xl text-gray-dark">
-                        Combine the power of Apache Spark with the quality from agile software engineering to transform your user data into
+                        Combine the power of Apache Spark with the quality from agile software engineering to transform
+                        your user data into
                         business intelligence to take your organization to the next level.
                     </p>
                 </div>
@@ -49,35 +50,44 @@ const Description = () => {
                             className="opacity-20 lg:opacity-100"
                         />
                     </div>
-                    <div className="absolute inset-0 lg:bg-blue mix-blend-multiply" />
+                    <div className="absolute inset-0 lg:bg-blue mix-blend-multiply"/>
                 </div>
             </div>
-            <div className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
+            <div
+                className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
                 <div className="lg:col-start-2 lg:pl-8">
                     <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
                         <h2 className="leading-6 text-red font-semibold tracking-wide uppercase">Description</h2>
                         <p className="mt-8 text-lg font-medium lg:text-gray-dark lg:font-normal">
                             Apache Spark is great for organizations who want their analytics
                             developed fast and executed fast. The problem is that large-scale distributed computing is
-                            always hard. It’s not like you can set a breakpoint on code running on multiple machines when
-                            things go wrong. Monitoring your analytics jobs and optimizing accordingly are important. You
+                            always hard. It’s not like you can set a breakpoint on code running on multiple machines
+                            when
+                            things go wrong. Monitoring your analytics jobs and optimizing accordingly are important.
+                            You
                             also have to rethink the way you approach concepts like architecture, security, and software
                             engineering techniques like testing and DevSecOps.
                         </p>
                         <p className="mt-8 text-lg font-medium lg:text-gray-dark lg:font-normal">
-                            <span className=" text-red font-semibold">Software Engineering with Apache Spark</span> teaches
+                            <span
+                                className=" text-red font-semibold">Software Engineering with Apache Spark</span> teaches
                             you how to leverage just enough Scala to integrate
-                            powerful data pipelines into your existing monolithic, microservices, and/or serverless architecture.
-                            You will also learn how your pipelines can benefit from the same testing, continuous integration,
-                            containerization with Docker, container orchestration with Kubernetes, and security hardening
+                            powerful data pipelines into your existing monolithic, microservices, and/or serverless
+                            architecture.
+                            You will also learn how your pipelines can benefit from the same testing, continuous
+                            integration,
+                            containerization with Docker, container orchestration with Kubernetes, and security
+                            hardening
                             as the others components of your enterprise.
                         </p>
                         <div className="mt-5 text-gray-dark">
                             <h3 className="font-semibold">What makes this course different</h3>
                             <p className="font-medium lg:text-gray-dark lg:font-normal">
-                                Apache Spark is a huge topic, and the typical Spark course tries to cover too much in too
+                                Apache Spark is a huge topic, and the typical Spark course tries to cover too much in
+                                too
                                 short a period, which leaves you overwhelmed. Meanwhile, it ignores any discussion of
-                                architectural patterns, security, and software engineering--the things that separate experiments
+                                architectural patterns, security, and software engineering--the things that separate
+                                experiments
                                 in your garage from critical pieces of your production architecture.
                             </p>
                             <p className="font-medium">
@@ -85,14 +95,17 @@ const Description = () => {
                                 into the API as you’d expect, but the exercises utilize real datasets from Data.gov and
                                 encourage you to collaborate with your peers, the Spark Scaladoc, Stack Overflow, and
                                 other sources to discover solutions not explicitly discussed–just as you would at work.
-                                We also advise you on where you should drawn the line between configuring Spark yourself and
+                                We also advise you on where you should drawn the line between configuring Spark yourself
+                                and
                                 outsourcing that to a provider so you can focus on what you care about.
                             </p>
                             <p>
                                 In addition, agile software development and DevOps have taught us the value of build
                                 automation, testing, continuous integration, and continuous delivery, and we don’t have
                                 to abandon them just because we are working in a distributed environment. In fact, the
-                                complexity of distributed systems makes them even more valuable. <span className=" text-red font-semibold">Software Engineering with Apache Spark</span> shows you how to apply these techniques to improve the quality and
+                                complexity of distributed systems makes them even more valuable. <span
+                                className=" text-red font-semibold">Software Engineering with Apache Spark</span> shows
+                                you how to apply these techniques to improve the quality and
                                 reliability of your analytics.
                             </p>
                         </div>
@@ -155,11 +168,14 @@ const Instructor = () => {
 
                 <div className="relative lg:flex lg:items-center">
                     <div className="hidden lg:block lg:flex-shrink-0">
-                        <img
-                            className="h-64 w-64 rounded-full xl:h-80 xl:w-80"
-                            src="/img/staff/president.png"
-                            alt=""
-                        />
+                        <div className="h-64 w-64 rounded-full xl:h-80 xl:w-80 relative">
+                            <Image
+                                layout="fill"
+                                className="rounded-full"
+                                src="/img/staff/president.png"
+                                alt="Course instructor Neil Chaudhuri"
+                            />
+                        </div>
                     </div>
 
                     <div className="relative lg:ml-10">
@@ -177,9 +193,12 @@ const Instructor = () => {
                         <blockquote className="relative">
                             <div className="text-2xl leading-9 font-medium text-gray-900">
                                 <p>
-                                    I have built several Scala and Apache Spark applications currently in production, and I
-                                    worked with the original Apache Spark team, AMPLab at UC Berkeley, on a research project
-                                    for DARPA known as XDATA. Somehow I have helped enough developers around the world to
+                                    I have built several Scala and Apache Spark applications currently in production,
+                                    and I
+                                    worked with the original Apache Spark team, AMPLab at UC Berkeley, on a research
+                                    project
+                                    for DARPA known as XDATA. Somehow I have helped enough developers around the world
+                                    to
                                     earn Spark and Scala badges on Stack Overflow. I am passionate about Spark and look
                                     forward to helping you harness its power.
                                 </p>
@@ -187,15 +206,22 @@ const Instructor = () => {
                             <footer className="mt-8">
                                 <div className="flex">
                                     <div className="flex-shrink-0 lg:hidden">
-                                        <img
-                                            className="h-12 w-12 rounded-full"
-                                            src="/img/staff/president.png"
-                                            alt=""
-                                        />
+                                        <div className="h-20 w-20 rounded-full relative">
+                                            <Image
+                                                layout="fill"
+                                                className="rounded-full"
+                                                src="/img/staff/president.png"
+                                                alt="Course instructor Neil Chaudhuri"
+                                            />
+                                        </div>
                                     </div>
                                     <div className="ml-4 lg:ml-0">
-                                        <div className="text-lg lg:text-xl font-medium text-gray-dark">Neil Chaudhuri</div>
-                                        <div className="text-base lg:text-lg font-medium text-red">Course instructor</div>
+                                        <div className="text-lg lg:text-xl font-medium text-gray-dark">
+                                            Neil Chaudhuri
+                                        </div>
+                                        <div className="text-base lg:text-lg font-medium text-red">
+                                            Course instructor
+                                        </div>
                                     </div>
                                 </div>
                             </footer>
@@ -206,8 +232,6 @@ const Instructor = () => {
         </section>
     )
 }
-
-
 
 
 export default Spark
