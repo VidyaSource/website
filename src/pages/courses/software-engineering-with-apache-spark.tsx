3a49ee798121961e4e47f4206f79bd63f52cd723
neilchaudhuri <neil@vidyasource.com>
2021-05-19 22:29:57 -0400

    Remove old CourseHeader

--- src/pages/courses/software-engineering-with-apache-spark.tsx
+++ src/pages/courses/software-engineering-with-apache-spark.tsx
@@ -19,26 +19,6 @@ export const Spark = () => {
     )
 }
 
-export const CourseHeader1 = () => {
-    return (
-        <div className="bg-blue-light">
-            <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-                <div className="text-center">
-                    <h1 className="text-base font-semibold text-green-dark tracking-wide uppercase">Data Science</h1>
-                    <h2 className="mt-1 text-4xl font-extrabold text-red sm:text-5xl sm:tracking-tight lg:text-6xl">
-                        Software Engineering with Apache Spark
-                    </h2>
-                    <p className="max-w-xl mt-5 mx-auto text-xl text-gray-dark">
-                        Combine the power of Apache Spark with the quality from agile software engineering to transform
-                        your user data into
-                        business intelligence to take your organization to the next level.
-                    </p>
-                </div>
-            </div>
-        </div>
-    )
-}
-
 const Description = () => {
     return (
         <div className="relative bg-white">
