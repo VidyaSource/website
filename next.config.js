1ca8454967650144667c9fcf77d0bb7b65bdc127
neilchaudhuri <neil@vidyasource.com>
2021-05-21 22:13:13 -0400

    Initial agile verbiage

--- next.config.js
+++ next.config.js
@@ -56,6 +56,46 @@ module.exports = {
                 "                                    to\n" +
                 "                                    earn Spark and Scala badges on Stack Overflow. I am passionate about Spark and look\n" +
                 "                                    forward to helping you harness its power."
-        }
+        },
+        agile: {
+            blurb: "We've all worked on projects that claim to be agile but are nothing more than the same old wasteful waterfall with daily standups. Learn how to be agile rather than merely do agile.",
+            description1: "It's been two decades since the Agile Manifesto, and its impact on software development has been so profound that  \n" +
+                "every project claims to be agile. There's a backlog. The whole team mumbles its way through pointless daily standups.\n" +
+                "You even do Fist of Five votes of confidence after long Sprint Planning meetings, but they're pure theater\n" +
+                "because your boss told you to keep bad news away from the client. In the end, you\n" +
+                "know full well there is nothing agile about your project. Sound familiar?",
+            description2: " teaches you how to move past performative ceremonies--to be agile not just do agile. You will learn that what matters \n" +
+                "most of all is outcomes. How you get there day to day depends on your organizational culture, but you will\n" +
+                "learn about the keys to delivering the highest quality software in the least amount of time spending the least amount of money: \n" +
+                "communication, psychological safety, and relentless feedback about your product, your client, your technology, your \n" +
+                "competitive landscape, and most importantly, yourselves.",
+            syllabus: {
+                "Session 1: Mastering the Spark API": [
+                    "MapReduce: The Phantom Menace",
+                    "Advantages of Spark",
+                    "Just Enough Scala",
+                    "Using the Spark Shell",
+                    "Writing You Own Spark Jobs",
+                    "The Spark Ecosystem"
+                ],
+                "Session 2: Professional Spark": [
+                    "Just Enough Hadoop",
+                    "Testing Your Spark Jobs",
+                    "Optimizing Spark and When to Stop Trying",
+                    "Spark on Docker",
+                    "Deploying Spark to Kubernetes",
+                    "Spark Security",
+                    "Visualizing Your Spark Jobs"
+                ],
+            },
+            quote: "I have built several Scala and Apache Spark applications currently in production,\n" +
+                "                                    and I\n" +
+                "                                    worked with the original Apache Spark team, AMPLab at UC Berkeley, on a research\n" +
+                "                                    project\n" +
+                "                                    for DARPA known as XDATA. Somehow I have helped enough developers around the world\n" +
+                "                                    to\n" +
+                "                                    earn Spark and Scala badges on Stack Overflow. I am passionate about Spark and look\n" +
+                "                                    forward to helping you harness its power."
+        },
     }
 }
