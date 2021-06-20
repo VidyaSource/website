6d8f43bb55f9cb3e3d957767974ee6616dba94f8
neilchaudhuri <neil@vidyasource.com>
2021-06-20 15:16:33 -0400

    Images

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -1,38 +1,37 @@
 /* This example requires Tailwind CSS v2.0+ */
 import {CourseHeadElement} from "../../components/courses/CourseHeadElement";
 import {Page} from "../../components/Page";
+import Image from "next/image";
+import Link from "next/link";
 
-const posts = [
+const courses = [
     {
         title: 'Java for Work',
         href: '/courses/java-for-work',
         // @ts-ignore
         description: process.env.java.blurb,
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        imageUrl: '/img/courses/programming.jpg',
     },
     {
         title: 'Modern Agile',
         href: '/courses/modern-agile',
         // @ts-ignore
         description: process.env.agile.blurb,
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        imageUrl: '/img/courses/agile.jpg',
     },
     {
         title: 'Software Engineering with Apache Spark',
         href: 'courses/software-engineering-with-apache-spark',
         // @ts-ignore
         description: process.env.spark.blurb,
-        imageUrl:
-            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        imageUrl: '/img/courses/spark.jpg',
     },
 ]
 
 export const Courses = () => {
     return (
         <Page headElement={<CourseHeadElement title="Vidya | Courses" />}>
-        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-16 lg:pb-28 lg:px-8">
             <div className="absolute inset-0">
                 <div className="bg-white h-1/3 sm:h-2/3" />
             </div>
@@ -40,21 +39,35 @@ export const Courses = () => {
                 <div className="text-center">
                     <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Courses</h2>
                     <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-                        Level up in your career with these course offerings from Vidya
+                        Level up in your career with these course offerings from Vidya.
                     </p>
                 </div>
                 <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-                    {posts.map((post) => (
-                        <div key={post.title} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                            <div className="flex-shrink-0">
-                                <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
-                            </div>
-                            <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                                <div className="flex-1">
-                                    <a href={post.href} className="block mt-2">
-                                        <p className="text-xl font-semibold text-gray-900">{post.title}</p>
-                                        <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                    {courses.map((course) => (
+                        <div key={course.title} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                            <div className="flex-shrink-0 bg-gray-light hover:bg-red-light">
+                                <Link href={course.href}>
+                                    <a>
+                                        <div className="h-48 w-full relative">
+                                            <Image
+                                                objectFit="cover"
+                                                layout="fill"
+                                                quality={100}
+                                                src={course.imageUrl}
+                                                alt={course.title}
+                                            />
+                                        </div>
                                     </a>
+                                </Link>
+                            </div>
+                            <div className="flex-1 bg-white p-6 flex flex-col justify-between hover:bg-red-light">
+                                <div className="flex-1">
+                                    <Link href={course.href}>
+                                    <a className="block mt-2">
+                                        <p className="text-xl font-semibold text-red">{course.title}</p>
+                                        <p className="mt-3 text-base text-gray-dark">{course.description}</p>
+                                    </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
