be90d436cc46e61a1b8bc750b9ff1de9e1c318c4
neilchaudhuri <neil@vidyasource.com>
2021-06-21 20:55:10 -0400

    Use common component

--- src/pages/tutorials/nine-reasons-to-try-scala.tsx
+++ src/pages/tutorials/nine-reasons-to-try-scala.tsx
@@ -1,40 +1,27 @@
-import {Page} from "../../components/Page";
-import {TutorialHeadElement} from "../../components/tutorials/TutorialHeadElement";
-import {TutorialHeader} from "../../components/tutorials/TutorialHeader";
-import {TutorialContent} from "../../components/tutorials/TutorialContent";
-import {YouTubeVideo} from "../../components/YouTubeVideo";
+import {Tutorial} from "../../components/tutorials/Tutorial";
 
 export const Scala = () => {
-    const name = "Nine Reasons to Try Scala"
-    // @ts-ignore
+    const title = "Nine Reasons to Try Scala"
     return (
-        <Page headElement={<TutorialHeadElement title={`Vidya | ${name}`} />}>
-            <TutorialHeader category="Scala" name={name} />
-            <TutorialContent title={name}>
-                <div>
-                    This tutorial is for intermediate-level Java developers, and developers in other languages too, who are curious about what the big deal is with the Scala programming language. We will look at nine compelling features of Scala that will hopefully impress you and inspire you to explore both the language itself and its applications.
+        <Tutorial title={title} github="https://github.com/VidyaSource/nine-reasons-to-try-scala" youTubeId="rbZ6GzR8B7I">
+            This tutorial is for intermediate-level Java developers, and developers in other languages too,
+            who are curious about what the big deal is with the Scala programming language. We will look at
+            nine compelling features of Scala that will hopefully impress you and inspire you to explore
+            both the language itself and its applications.
 
-                    If you want to skip to sections that particularly interest you, here is where you should go:
-                    <ol>
-                        <li>Immutability (1:13)</li>
-                        <li>Traits (2:52)</li>
-                        <li>Case classes (5:59)</li>
-                        <li>Extractors and pattern matching (7:22)</li>
-                        <li>Option (8:36)</li>
-                        <li>Implicit conversion (11:46)</li>
-                        <li>Parallel collections (12:46)</li>
-                        <li>Futures (13:32)</li>
-                        <li>Popularity (15:22)</li>
-                    </ol>
-                    <div>
-                        If you want to skip the video altogether, <a href="https://github.com/VidyaSource/nine-reasons-to-try-scala">the source is on GitHub</a>.
-                    </div>
-                    <div className="pt-16">
-                        <YouTubeVideo id="rbZ6GzR8B7I" title="Nine Reasons to Try Scala" />
-                    </div>
-                </div>
-            </TutorialContent>
-        </Page>
+            If you want to skip to sections that particularly interest you, here is where you should go:
+            <ol>
+                <li>Immutability (1:13)</li>
+                <li>Traits (2:52)</li>
+                <li>Case classes (5:59)</li>
+                <li>Extractors and pattern matching (7:22)</li>
+                <li>Option (8:36)</li>
+                <li>Implicit conversion (11:46)</li>
+                <li>Parallel collections (12:46)</li>
+                <li>Futures (13:32)</li>
+                <li>Popularity (15:22)</li>
+            </ol>
+        </Tutorial>
     )
 }
 
---
+++ src/pages/tutorials/starting-with-data.tsx
@@ -0,0 +1,13 @@
+import {Tutorial} from "../../components/tutorials/Tutorial";
+
+export const Scala = () => {
+    const title = "Starting with Data"
+    return (
+        <Tutorial title={title} github="https://github.com/VidyaSource/starting-with-data" youTubeId="bzl4hCH2CdY">
+            This tutorial is for beginners in software development who want to learn just enough to access data on the web
+            and visualize it on their own websites or mobile device applications.
+        </Tutorial>
+    )
+}
+
+export default Scala
