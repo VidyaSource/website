c5232319855656ee9e39fc038acea81365381c0d
neilchaudhuri <neil@vidyasource.com>
2021-06-26 17:28:35 -0400

    All components

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,19 @@
-import {TutorialHeadElement} from "../components/tutorials/TutorialHeadElement";
 import {Page} from "../components/Page";
 import {Hero} from "../components/about/Hero";
+import {AboutHeadElement} from "../components/about/AboutHeadElement";
+import Vidya from "../components/about/Vidya";
+import {Clients} from "../components/Clients";
+import Diversity from "../components/about/Diversity";
+import Offerings from "../components/about/Offerings";
 
 const About = () => {
     return (
-        <Page headElement={<TutorialHeadElement title="Vidya | About" />}>
+        <Page headElement={<AboutHeadElement title="Vidya | About" />}>
             <Hero />
+            <Offerings />
+            <Diversity />
+            <Vidya />
+            <Clients />
         </Page>
     )
 }
