5024402fa6df5f1d0d3a77c3d99f97ae233cc356
neilchaudhuri <neil@vidyasource.com>
2021-06-27 16:56:47 -0400

    Simple mailer function

---
+++ src/pages/api/mail.js
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const nodemailer = require("nodemailer")
+const vidya = "vidyacontactinfo@gmail.com"
+
+export default async (req, res) => {
+  //res.status(200).json({ name: 'John Doe' })
+  const {from, subject, message} = req
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: vidya,
+      pass: process.env.MAIL_PASSWORD
+    },
+  })
+  const info = await transporter.sendMail({
+    from: from,
+    to: vidya,
+    subject: subject,
+    text: message
+  })
+}
+
