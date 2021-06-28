f84f8ff9654791aa5e6dedd33203aab0e0ca1acf
neilchaudhuri <neil@vidyasource.com>
2021-06-27 23:53:35 -0400

    Focus green

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -269,7 +269,7 @@ const Form = () => {
                                     <div className="sm:col-span-2 sm:flex sm:justify-end">
                                         <button
                                             onClick={submit}
-                                            className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red hover:bg-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white0 sm:w-auto"
+                                            className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue hover:bg-green focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white0 sm:w-auto"
                                         >
                                             Submit
                                         </button>
