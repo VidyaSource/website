fa412a4ad74d429b03db8265dfa186c63f010374
neilchaudhuri <neil@vidyasource.com>
2021-06-30 14:05:13 -0400

    Add CallToAction

--- src/pages/consulting.tsx
+++ src/pages/consulting.tsx
@@ -2,12 +2,14 @@ import {Page} from "../components/Page";
 import {ConsultingHeadElement} from "../components/consulting/ConsultingHeadElement";
 import {Architecture} from "../components/consulting/Architecture";
 import Communications from "../components/consulting/Communications";
+import {CallToAction} from "../components/CallToAction";
 
 const Consulting = () => {
     return (
         <Page headElement={<ConsultingHeadElement title="Vidya | Consulting" />}>
             <Architecture />
             <Communications />
+            <CallToAction />
         </Page>
     )
 }
