e3b708975c292827a6096871ed267119ac71fbba
neilchaudhuri <neil@vidyasource.com>
2021-06-30 13:50:53 -0400

    Add comms

--- src/pages/consulting.tsx
+++ src/pages/consulting.tsx
@@ -1,11 +1,13 @@
 import {Page} from "../components/Page";
 import {ConsultingHeadElement} from "../components/consulting/ConsultingHeadElement";
 import {Architecture} from "../components/consulting/Architecture";
+import Communications from "../components/consulting/Communications";
 
 const Consulting = () => {
     return (
         <Page headElement={<ConsultingHeadElement title="Vidya | Consulting" />}>
             <Architecture />
+            <Communications />
         </Page>
     )
 }
