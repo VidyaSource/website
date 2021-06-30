ff04331ecdb58ef321af126315dd1f93debe1a67
neilchaudhuri <neil@vidyasource.com>
2021-06-30 16:54:16 -0400

    Use dynamic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import {HeadElement} from "../components/HeadElement";
 import {Hero} from "../components/home/Hero"
 import {RecentPosts} from "../components/home/RecentPosts"
-import {Footer} from "../components/Footer";
-import {CallToAction} from "../components/CallToAction";
-import {Testimonials} from "../components/home/Testimonials";
-import {Clients} from "../components/Clients";
 import {QueryClient} from "react-query";
 import {getAllBlogPosts} from "../lib/blogPost-utils";
 import {dehydrate} from "react-query/hydration";
+import dynamic from 'next/dynamic'
+
+const Clients = dynamic(() => import("../components/Clients"))
+const Testimonials = dynamic(() => import("../components/home/Testimonials"))
+const CallToAction = dynamic(() => import("../components/CallToAction"))
+const Footer = dynamic(() => import("../components/Footer"))
 
 const Home = () => {
     return (
