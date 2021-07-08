d1662a2f8a502a28124b4efee8743373d018ec12
neilchaudhuri <neil@vidyasource.com>
2021-07-07 21:26:41 -0400

    Use TS

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import '../../styles/globals.css'
+import { DefaultSeo } from 'next-seo'
+import seo from '../../seo.config'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import ReactGA from "react-ga4"
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <DefaultSeo {...seo} />
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+    const body = JSON.stringify(metric)
+    ReactGA.initialize("G-SV6CZCPD3B")
+    ReactGA.send(body)
+}
+
+export default MyApp
