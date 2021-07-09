9cc727683bdf84849114e338d205fe1406af5cce
neilchaudhuri <neil@vidyasource.com>
2021-07-08 21:11:23 -0400

    Use Tutorial

--- src/pages/tutorials/comparison-shopping.tsx
+++ src/pages/tutorials/comparison-shopping.tsx
@@ -1,11 +1,25 @@
 import {Tutorial} from "../../components/tutorials/Tutorial";
 
-export const ComparisonShopping = () => {
+const ComparisonShopping = () => {
+    const metaDescription = "This tutorial is for intermediate-level Java developers who want to add comparison and sorting capabilities to the custom classes they’ve created for their projects."
+    const thumbnail = {
+        url: "/img/tutorials/car-shopping.jpg",
+        width: 3840,
+        height: 2298
+    }
     return (
-        <Tutorial title="Comparison Shopping" subtitle="Using Comparable and Comparator to Sort Java Objects"
-                  github="https://github.com/VidyaSource/comparison-shopping" youTubeId="pq0ArQhhWzU">
-            This tutorial is for intermediate-level Java developers who want to add comparison and sorting capabilities to the custom classes they’ve created for their projects.
+        <Tutorial title="Comparison Shopping"
+                  subtitle="Using Comparable and Comparator to Sort Java Objects"
+                  metaDescription={metaDescription}
+                  github="https://github.com/VidyaSource/comparison-shopping"
+                  youTubeId="pq0ArQhhWzU"
+                  tags={["Java", "programming", "sorting", "comparator", "comparable", "function"]}
+                  uploadDate={new Date("2013-10-27")}
+                  thumbnail={thumbnail}>
+            This tutorial is for intermediate-level Java developers who want to add comparison and sorting capabilities
+            to the custom classes they’ve created for their projects.
         </Tutorial>
+
     )
 }
 
--- src/pages/tutorials/starting-with-data.tsx
+++ src/pages/tutorials/starting-with-data.tsx
@@ -1,12 +1,23 @@
 import {Tutorial} from "../../components/tutorials/Tutorial";
 
 export const StartingWithData = () => {
-    const title = "Starting with Data"
+    const metaDescription = "This tutorial is for beginners in software development who want to learn just enough to access data on the web and visualize it on their own websites or mobile devices."
+    const thumbnail = {
+        url: "/img/tutorials/data.jpg",
+        width: 4810,
+        height: 3207
+    }
     return (
-        <Tutorial title="Starting with Data" github="https://github.com/VidyaSource/starting-with-data" youTubeId="bzl4hCH2CdY"
-                  subtitle="Using Python and D3 to Visualize JSON from the Donors Choose REST API">
+        <Tutorial title="Starting with Data"
+                  github="https://github.com/VidyaSource/starting-with-data"
+                  youTubeId="bzl4hCH2CdY"
+                  subtitle="Using Python and D3 to Visualize JSON from the Donors Choose REST API"
+                  tags={["data", "Python", "data visualization", "dataviz", "Donor's Choose", "API", "REST", "Flask"]}
+                  uploadDate={new Date("2014-01-14")}
+                  thumbnail={thumbnail}
+                  metaDescription={metaDescription}>
             This tutorial is for beginners in software development who want to learn just enough to access data on the web
-            and visualize it on their own websites or mobile device applications.
+            and visualize it on their own websites or mobile devices.
         </Tutorial>
     )
 }
