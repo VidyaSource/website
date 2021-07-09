bd76fb382c75a447f7aa148e77bd1ff7be0d8e04
neilchaudhuri <neil@vidyasource.com>
2021-07-08 21:09:17 -0400

    SEO

--- src/pages/consulting.tsx
+++ src/pages/consulting.tsx
@@ -1,18 +1,48 @@
 import {Page} from "../components/Page";
-import {ConsultingHeadElement} from "../components/consulting/ConsultingHeadElement";
 import {Architecture} from "../components/consulting/Architecture"
 import dynamic from 'next/dynamic'
+import {NextSeo, ProductJsonLd} from "next-seo";
+import {useRouter} from "next/router";
 
 const Communications = dynamic(() => import("../components/consulting/Communications"))
 const CallToAction = dynamic(() => import("../components/CallToAction"))
 
 const Consulting = () => {
+    const router = useRouter()
+    const title = "Vidya | Consulting"
+    const description = "Trust Vidya to bring the experience and skill to solve your software architecture, software development, and technology communications challenges."
+    const url = `${process.env.basePath}${router.pathname}`
     return (
-        <Page headElement={<ConsultingHeadElement title="Vidya | Consulting" />}>
-            <Architecture />
-            <Communications />
-            <CallToAction />
-        </Page>
+        <>
+            <NextSeo
+                title={title}
+                description={description}
+                canonical={url}
+                openGraph={{
+                    title: title,
+                    images: [
+                        {
+                            url: `${process.env.basePath}/img/consulting/development.jpg`,
+                            width: 3394,
+                            height: 4243,
+                            alt: title,
+                        }
+                    ],
+                    description: description,
+                    url: url,
+                    type: 'product',
+                }}
+            />
+            <ProductJsonLd productName="Vidya Consulting"
+                           description={description}
+                           slogan="Build Faster. Build Better."
+                           brand="Vidya"/>
+            <Page>
+                <Architecture/>
+                <Communications/>
+                <CallToAction/>
+            </Page>
+        </>
     )
 }
 
