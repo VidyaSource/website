1d04e92987e886727faa9bfb0856ac0c8cb1be1a
neilchaudhuri <neil@vidyasource.com>
2021-07-10 15:59:44 -0400

    Validator/sanitizer

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,27 +1,33 @@
-const nodemailer = require("nodemailer")
-const vidya = "vidyacontactinfo@gmail.com"
+import nodemailer from 'nodemailer'
+import isEmail from 'validator/lib/isEmail'
+import DOMPurify from 'dompurify'
+
+const vidyaFrom = "vidyacontactinfo@gmail.com"
+const vidyaTo = "info@vidyasource.com"
 
 export default async (req, res) => {
   const {from, subject, message} = req.body
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: vidya,
-      pass: process.env.MAIL_PASSWORD
-    },
-  })
-  await transporter.sendMail({
-    from: vidya,
-    to: "info@vidyasource.com",
-    subject: subject,
-    text: `From ${from}\n\n${message}`
-  }, function (err, info) {
-    if(err)
-      res.status(err.responseCode).json({message: err.response})
-    else
-      res.status(200).json({message: "OK"})
-  })
+  if (isEmail(from)) {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: vidyaFrom,
+        pass: process.env.MAIL_PASSWORD
+      },
+    })
+    await transporter.sendMail({
+      from: vidyaFrom,
+      to: vidyaTo,
+      subject: DOMPurify.sanitize(subject),
+      text: `From ${from}\n\n${DOMPurify.sanitize(message)}`
+    }, function (err, info) {
+      if (err)
+        res.status(err.responseCode).json({message: err.response})
+      else
+        res.status(200).json({message: `OK: ${info}`})
+    })
+  }
 }
 
