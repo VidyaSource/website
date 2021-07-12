b81c477ca6aa377ff98a6589f1b877cbfdb5a625
neilchaudhuri <neil@vidyasource.com>
2021-07-11 20:09:21 -0400

    Blue image

--- src/components/about/Hero.tsx
+++ src/components/about/Hero.tsx
@@ -7,24 +7,30 @@ const Hero = () => {
                 <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
                     <section className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
                         <h1 className="text-4xl tracking-tight font-extrabold text-red dark:text-red-light sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-                            <span className="block xl:inline">Vidya is a software services and consulting company</span>{' '}
+                            <span
+                                className="block xl:inline">Vidya is a software services and consulting company</span>{' '}
                             <span className="block text-blue dark:text-blue-light xl:inline">based in Washington, DC, USA</span>
                         </h1>
                         <p className="mt-3 max-w-md mx-auto text-lg text-gray-dark dark:text-blue-light sm:text-xl md:mt-5 md:max-w-3xl">
-                            We are a certified Minority Business Enterprise by the Capital Region Minority Supplier Development Council and a certified Asian and minority-owned small business by the US Pan Asian American Chamber of Commerce.
+                            We are a certified Minority Business Enterprise by the Capital Region Minority Supplier
+                            Development Council and a certified Asian and minority-owned small business by the US Pan
+                            Asian American Chamber of Commerce.
                         </p>
                     </section>
                 </div>
-                <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
-                        <Image
-                            priority={true}
-                            loading="eager"
-                            objectFit="cover"
-                            layout="fill"
-                            quality={100}
-                            src="/img/about/computers.jpg"
-                            alt="About Vidya"
-                        />
+                <div
+                    className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full bg-blue z-30">
+                    <Image
+                        priority={true}
+                        loading="eager"
+                        objectFit="cover"
+                        layout="fill"
+                        quality={100}
+                        src="/img/about/computers.jpg"
+                        alt="About Vidya"
+
+                    />
+                    <div className="absolute inset-0 bg-blue mix-blend-multiply"/>
                 </div>
             </div>
         </div>
