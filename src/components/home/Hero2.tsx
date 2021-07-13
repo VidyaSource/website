c6654e16bce2aa9fbab0bed74f4c1a8e3eb7c28e
neilchaudhuri <neil@vidyasource.com>
2021-07-13 18:43:14 -0400

    Adjust pattern

--- src/components/home/Hero2.tsx
+++ src/components/home/Hero2.tsx
@@ -22,7 +22,7 @@ export default function Hero2() {
                         width={404}
                         height={784}
                         fill="none"
-                        viewBox="0 0 404 784"
+                        viewBox="0 0 404 484"
                     >
                         <defs>
                             <pattern
@@ -33,17 +33,17 @@ export default function Hero2() {
                                 height={20}
                                 patternUnits="userSpaceOnUse"
                             >
-                                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                                <rect x={0} y={0} width={4} height={4} className="text-green" fill="currentColor" />
                             </pattern>
                         </defs>
-                        <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+                        <rect width={404} height={184} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
                     </svg>
                     <svg
                         className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
                         width={404}
-                        height={784}
+                        height={484}
                         fill="none"
-                        viewBox="0 0 404 784"
+                        viewBox="0 0 404 484"
                     >
                         <defs>
                             <pattern
@@ -54,7 +54,7 @@ export default function Hero2() {
                                 height={20}
                                 patternUnits="userSpaceOnUse"
                             >
-                                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                                <rect x={0} y={0} width={4} height={4} className="text-red-light" fill="currentColor" />
                             </pattern>
                         </defs>
                         <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
