103e2b1dfa4ad8aec945c66fa71da6d620f184cd
neilchaudhuri <neil@vidyasource.com>
2021-07-13 21:00:01 -0400

    Popover and color adjustment

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,6 +1,11 @@
+/* This example requires Tailwind CSS v2.0+ */
+import {Fragment, useContext} from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import Image from "next/image";
 import Link from "next/link";
 import {ThemeModeLinkButton} from "../ThemeModeButton";
+import {DarkModeContext} from "../ThemeModeContext";
 
 const navigation = [
     {name: 'Courses', href: '/courses'},
@@ -10,6 +15,7 @@ const navigation = [
 ]
 
 export const Hero = () => {
+    const {darkMode, setDarkMode} = useContext(DarkModeContext)
     return (
         <div className="relative bg-blue-light dark:bg-blue overflow-hidden">
             <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
@@ -19,7 +25,8 @@ export const Hero = () => {
                         width={404}
                         height={784}
                         fill="none"
-                        viewBox="0 0 404 784">
+                        viewBox="0 0 404 484"
+                    >
                         <defs>
                             <pattern
                                 id="f210dbf6-a58d-4871-961e-36d5016a0f49"
@@ -27,18 +34,20 @@ export const Hero = () => {
                                 y={0}
                                 width={20}
                                 height={20}
-                                patternUnits="userSpaceOnUse">
-                                <rect x={0} y={0} width={4} height={4} className="text-red dark:text-red-light" fill="currentColor"/>
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <rect x={0} y={0} width={4} height={4} className="text-red dark:text-red-light" fill="currentColor" />
                             </pattern>
                         </defs>
-                        <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"/>
+                        <rect width={404} height={184} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
                     </svg>
                     <svg
                         className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
                         width={404}
-                        height={784}
+                        height={484}
                         fill="none"
-                        viewBox="0 0 404 784">
+                        viewBox="0 0 404 484"
+                    >
                         <defs>
                             <pattern
                                 id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
@@ -46,46 +55,113 @@ export const Hero = () => {
                                 y={0}
                                 width={20}
                                 height={20}
-                                patternUnits="userSpaceOnUse">
-                                <rect x={0} y={0} width={4} height={4} className="text-red dark:text-red-light" fill="currentColor"/>
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <rect x={0} y={0} width={4} height={4} className="text-red dark:text-red-light" fill="currentColor" />
                             </pattern>
                         </defs>
-                        <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"/>
+                        <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
                     </svg>
                 </div>
             </div>
 
             <div className="relative pt-6 pb-16 sm:pb-24">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6">
-                    <nav className="relative flex items-center justify-between sm:h-10 md:justify-center"
-                         aria-label="Global">
-                        <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-                            <div className="flex items-center justify-between w-full md:w-auto">
-                                <Link href="/">
-                                    <a className="logo">
-                                        <span className="sr-only">Vidya</span>
-                                        <Image src="/img/vidya.png"
-                                               alt="Vidya"
-                                               height={65}
-                                               width={140}
-                                               quality={100}
-                                               priority={true}/>
-                                    </a>
-                                </Link>
+                <Popover>
+                    {({ open }) => (
+                        <>
+                            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+                                <nav
+                                    className="relative flex items-center justify-between sm:h-10 md:justify-center"
+                                    aria-label="Global"
+                                >
+                                    <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+                                        <div className="flex items-center justify-between w-full md:w-auto">
+                                            <Link href="/">
+                                                <a className="logo">
+                                                    <span className="sr-only">Vidya</span>
+                                                    <Image src="/img/vidya.png"
+                                                           alt="Vidya"
+                                                           height={65}
+                                                           width={140}
+                                                           quality={100}
+                                                           priority={true}/>
+                                                </a>
+                                            </Link>
+                                            <div className="-mr-2 flex items-center md:hidden">
+                                                <Popover.Button className="bg-blue-light rounded-md p-2 inline-flex items-center justify-center text-gray hover:text-blue hover:bg-gray-light focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray">
+                                                    <span className="sr-only">Open main menu</span>
+                                                    <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                                                </Popover.Button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="hidden md:flex md:space-x-10">
+                                        {navigation.map((item) => (
+                                            <Link href={item.href} key={item.name}>
+                                                <a className="font-medium text-blue-dark hover:text-red dark:text-red-light">
+                                                    {item.name}
+                                                </a>
+                                            </Link>
+                                        ))}
+                                        <ThemeModeLinkButton />
+                                    </div>
+                                </nav>
                             </div>
-                        </div>
-                        <div className="hidden md:flex md:space-x-10 md:text-base lg:text-lg">
-                            {navigation.map((item) => (
-                                <Link href={item.href} key={item.name}>
-                                    <a className="font-medium text-blue-dark hover:text-red dark:text-red-light">
-                                        {item.name}
-                                    </a>
-                                </Link>
-                            ))}
-                            <ThemeModeLinkButton />
-                        </div>
-                    </nav>
-                </div>
+
+                            <Transition
+                                show={open}
+                                as={Fragment}
+                                enter="duration-150 ease-out"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="duration-100 ease-in"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <Popover.Panel
+                                    focus
+                                    static
+                                    className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                                >
+                                    <div className="rounded-lg shadow-md bg-white dark:bg-gray ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                        <div className="px-5 pt-4 flex items-center justify-between">
+                                            <div className="logo">
+                                                <span className="sr-only">Vidya</span>
+                                                <Image src="/img/vidya.png"
+                                                       alt="Vidya"
+                                                       height={56}
+                                                       width={120}
+                                                       quality={100}
+                                                       priority={true}/>
+
+                                            </div>
+                                            <div className="-mr-2">
+                                                <Popover.Button className="mr-4 bg-white bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray hover:text-red hover:bg-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray dark:bg-gray dark:hover:bg-gray-dark">
+                                                    <ThemeModeLinkButton  />
+                                                </Popover.Button>
+                                                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray hover:text-red hover:bg-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red dark:bg-gray dark:text-white dark:hover:bg-white dark:hover:text-blue">
+                                                    <span className="sr-only">Close menu</span>
+                                                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                                                </Popover.Button>
+                                            </div>
+                                        </div>
+                                        <div className="px-2 pt-2 pb-3">
+                                            {navigation.map((item) => (
+                                                <a
+                                                    key={item.name}
+                                                    href={item.href}
+                                                    className="block px-3 py-2 rounded-md text-base font-medium text-blue hover:text-white hover:bg-blue focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue dark:text-blue-light dark:hover:bg-red dark:focus:ring-blue-light"
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </Popover.Panel>
+                            </Transition>
+                        </>
+                    )}
+                </Popover>
 
                 <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
                     <section className="text-center">
