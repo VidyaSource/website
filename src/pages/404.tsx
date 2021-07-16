e378d54fb888c3069d14e9b9aa23c3be82e7713f
neilchaudhuri <neil@vidyasource.com>
2021-07-15 22:30:42 -0400

    Dark mode

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -18,17 +18,17 @@ const NotFound = () => {
                     }}
                 >
                     <div className="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
-                        <p className="text-sm font-semibold text-gray-dark text-opacity-75 uppercase tracking-wide">404 error</p>
-                        <h1 className="mt-2 text-4xl font-extrabold text-red tracking-tight sm:text-5xl">
+                        <p className="text-sm font-semibold text-gray-dark dark:text-gray-dark text-opacity-75 uppercase tracking-wide">404 error</p>
+                        <h1 className="mt-2 text-4xl font-extrabold text-red dark:text-red tracking-tight sm:text-5xl">
                             Uh oh! I think you’re lost.
                         </h1>
-                        <p className="mt-2 text-lg font-medium text-gray-dark text-opacity-75">
+                        <p className="mt-2 text-lg font-medium text-gray-dark dark:text-gray-dark text-opacity-75">
                             It looks like the page you’re looking for doesn't exist.
                         </p>
                         <div className="mt-6">
                             <a
                                 href="/"
-                                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue bg-white hover:bg-red-light hover:text-gray-dark">
+                                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue dark:text-white bg-white dark:bg-red hover:bg-red-light hover:text-gray-dark dark:hover:bg-blue">
                                 Go back home
                             </a>
                         </div>
