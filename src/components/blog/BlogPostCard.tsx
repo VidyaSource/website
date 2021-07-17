b978ac019f6ba050cdfb5d236e273d384e7f58c7
neilchaudhuri <neil@vidyasource.com>
2021-07-16 21:44:43 -0400

    Remove unused import

--- src/components/blog/BlogPostCard.tsx
+++ src/components/blog/BlogPostCard.tsx
@@ -1,9 +1,9 @@
-import {BlogPost, BlogPostMetadata} from "../../lib/blogPost-utils";
+import {BlogPostMetadata} from "../../lib/blogPost-utils";
 import {constants} from "../../lib/constants";
 import format from "date-fns/format";
 import Image from "next/image";
 import {MouseEventHandler, useMemo} from "react";
-import {selectTags} from "../../lib/selectTags";
+import {selectTags} from "../../lib/se˚˚lectTags";
 import Link from 'next/link'
 
 interface BlogPostCardProps {
