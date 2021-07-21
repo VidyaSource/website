0b731ace8142d46af0d0c810791675077e7d7923
neilchaudhuri <neil@vidyasource.com>
2021-07-21 17:38:39 -0400

    Zeitlin

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -52,9 +52,7 @@ const Testimonials = () => {
                                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                             </svg>
                             <p className="relative">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-                                molestiae. Numquam corrupti in laborum sed rerum et corporis. Nemo expedita voluptas culpa sapiente
-                                alias molestiae.
+                                If you are looking to accelerate your transition to a modernized, serverless architecture, you cannot find a better partner than Vidya. Their team really understands what the current technology climate is so as to bring us the best of breed solutions.
                             </p>
                         </div>
                         <footer className="mt-8">
@@ -64,13 +62,14 @@ const Testimonials = () => {
                                         width={80}
                                         height={80}
                                         className="inline-block rounded-full"
-                                        src="/img/staff/president.png"
-                                        alt=""
+                                        src="/img/testimonials/zeitlin.jpg"
+                                        alt="Michael Zeitlin, Principal, Sikor Software Inc."
+                                        quality={50}
                                     />
                                 </div>
                                 <div className="ml-4">
-                                    <div className="text-base font-medium text-red dark:text-red-light">Joseph Rodriguez</div>
-                                    <div className="text-base font-medium text-gray-dark dark:text-blue-light">CEO, Workcation</div>
+                                    <div className="text-base font-medium text-red dark:text-red-light">Michael Zeitlin</div>
+                                    <div className="text-base font-medium text-gray-dark dark:text-blue-light">Principal, Sikor Software Inc.</div>
                                 </div>
                             </div>
                         </footer>
