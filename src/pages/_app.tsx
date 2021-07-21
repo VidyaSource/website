6ff134baa6546343fd2e42dbc5daee8c4f557bb2
neilchaudhuri <neil@vidyasource.com>
2021-07-20 23:51:51 -0400

    Use React GA4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,25 +14,14 @@ function MyApp({Component, pageProps}: AppProps) {
             mode = localStorage.getItem('vidyaDarkMode') === 'true' || (!('vidyaDarkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
         }
     }, [])
+    useEffect(() => {
+        ReactGA.initialize("GTM-PFPX26R")
+    }, [])
     const [darkMode, setDarkMode] = useState(mode)
     return (
         <>
             <DefaultSeo {...seo} />
             <Script strategy="beforeInteractive" src="/scripts/darkMode.js"/>
-            <Script>
-                {
-                    `import Analytics from 'analytics'
-                      import googleTagManager from '@analytics/google-tag-manager'
-                      const analytics = Analytics({
-                      app: 'vidya-website',
-                      plugins: [
-                        googleTagManager({
-                          containerId: 'GTM-PFPX26R'
-                        })
-                      ]
-                    })`
-                }
-            </Script>
             <Script src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b9ebbff15106c8f" strategy="lazyOnload"/>
             <ThemeContext.Provider value={{darkMode: darkMode, setDarkMode: setDarkMode}}>
                 <Component {...pageProps} />
