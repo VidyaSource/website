cbca554fa2bff127150b275b6dd527a5ee4326e5
neilchaudhuri <neil@vidyasource.com>
2021-11-06 16:49:26 -0400

    Upgrade to Next 12

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
-    target: 'serverless',
+    swcMinify: true,
     pwa: {
         dest: 'public',
         disable: process.env.NODE_ENV === 'development'
