809ff99a0fb05a22f35518ed6f922b79e0cf7f13
neilchaudhuri <neil@vidyasource.com>
2021-11-09 20:32:10 -0500

    Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({Component, pageProps}: AppProps) {
             <DefaultSeo {...seo} />
             <Script strategy="beforeInteractive" src="/scripts/darkMode.js"/>
             <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-42673091-1" strategy="lazyOnload" />
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SV6CZCPD3B" />
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SV6CZCPD3B" strategy="lazyOnload" />
             <Script strategy="lazyOnload">
                 {
                     `window.dataLayer = window.dataLayer || [];
