9cbfc09df93ad28345a09629f75da903790f3aef
neilchaudhuri <neil@vidyasource.com>
2022-04-30 12:57:31 -0400

    Use light gray

--- src/components/home/RecentPosts.tsx
+++ src/components/home/RecentPosts.tsx
@@ -11,7 +11,7 @@ interface RecentPostsProps {
 const RecentPosts = (p: RecentPostsProps) => {
     const posts = p.blogPosts
     return (
-        <div className="relative bg-white dark:bg-blue-light pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="relative bg-gray-light dark:bg-blue-light pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
             <div className="absolute inset-0">
                 <div className="bg-white dark:bg-gray h-1/3 sm:h-2/3"/>
             </div>
