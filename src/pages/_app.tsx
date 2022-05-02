716522db8980633ee1274243449a0ea72bf9ddbd
neilchaudhuri <neil@vidyasource.com>
2022-05-02 14:02:22 -0400

    Revert

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,13 +13,23 @@ function MyApp({Component, pageProps}: AppProps) {
         <>
             <DefaultSeo {...seo} />
             <Script strategy="beforeInteractive" src="/scripts/darkMode.js"/>
-
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-42673091-1" strategy="lazyOnload" />
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SV6CZCPD3B" strategy="lazyOnload" />
+            <Script strategy="afterInteractive">
+                {
+                    `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-42673091-1')
+                    gtag('config', 'G-SV6CZCPD3B');`
+                }
+            </Script>
             <Component {...pageProps} />
         </>
     )
 }
 
-/*export function reportWebVitals(metric: NextWebVitalsMetric) {
+export function reportWebVitals(metric: NextWebVitalsMetric) {
     const { id, name, startTime, label, value } = metric
     ReactGA.initialize("G-SV6CZCPD3B")
     ReactGA.event({
@@ -30,6 +40,6 @@ function MyApp({Component, pageProps}: AppProps) {
         nonInteraction: true,
         transport: "beacon"
     })
-}*/
+}
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,23 @@ class VidyaDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-
+                    <Script strategy="afterInteractive">
+                        {
+                            `(function (w, d, s, l, i) {
+                            w[l] = w[l] || [];
+                            w[l].push({
+                                'gtm.start':
+                                    new Date().getTime(), event: 'gtm.js'
+                            });
+                            var f = d.getElementsByTagName(s)[0],
+                                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                            j.async = true;
+                            j.src =
+                                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                            f.parentNode.insertBefore(j, f);
+                        })(window, document, 'script', 'dataLayer', 'GTM-PFPX26R');`
+                        }
+                    </Script>
                     <link rel="shortcut icon" href="/img/favicon.ico"/>
                     <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
@@ -54,6 +70,7 @@ class VidyaDocument extends Document {
                     {/* PWA */}
                 </Head>
                 <body>
+                    <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFPX26R" height="0" width="0" style={{display:"none",visibility:"hidden"}} />`}} />
                     <Main/>
                     <NextScript/>
                 </body>
