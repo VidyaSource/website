e95dc43f82578bdfee1cc7e8c57e2f1cc0dcf482
neilchaudhuri <neil@vidyasource.com>
2022-05-03 23:04:50 -0400

    Initial

--- src/components/blog/BlogSearch.tsx
+++ src/components/blog/BlogSearch.tsx
@@ -1,93 +1,82 @@
-import {Fragment, useState} from 'react'
-import {SearchIcon} from '@heroicons/react/solid'
-import {Combobox, Dialog, Transition} from '@headlessui/react'
+import {ChangeEvent, useState} from 'react'
+import {CheckIcon, SelectorIcon} from '@heroicons/react/solid'
+import {Combobox} from '@headlessui/react'
 
 const people = [
-    {id: 1, name: 'Leslie Alexander', url: '#'},
-    // More people...
+    {id: 1, name: 'Leslie Alexander'},
+    // More users...
 ]
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function BlogSearch() {
-    const [query, setQuery] = useState('')
+interface BlogSearchProps {
+    tags: Array<string>
+    selectedTag: string
+    onChange: (s: string) => void
+}
 
-    const [open, setOpen] = useState(true)
-
-    const filteredPeople =
+export default function BlogSearch(p: BlogSearchProps) {
+    const [query, setQuery] = useState("")
+    const [selectedPerson, setSelectedPerson] = useState()
+    const values = p.tags.map(t => ({key: t, value: t}))
+    const filteredTags =
         query === ''
-            ? []
-            : people.filter((person) => {
-                return person.name.toLowerCase().includes(query.toLowerCase())
+            ? values
+            : values.filter((v) => {
+                return v.value.toLowerCase().includes(query.toLowerCase())
             })
 
     return (
-        <div></div>
-        /*<Transition.Root show={open} as={Fragment} afterLeave={() => setQuery('')}>
-            <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20" onClose={setOpen}>
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity"/>
-                </Transition.Child>
+        <Combobox as="div" value={p.selectedTag} onChange={p.onChange}>
+            <Combobox.Label className="block text-sm font-medium text-gray-700">Filter by tag</Combobox.Label>
+            <div className="relative mt-1">
+                <Combobox.Input
+                    className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+                    onChange={(e) => setQuery(e.target.value)}
+                    displayValue={(s) => s}
+                />
+                <Combobox.Button
+                    className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                    <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                </Combobox.Button>
 
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                >
-                    <Combobox
-                        as="div"
-                        className="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all"
-                        onChange={(person) => (window.location = person.url)}
-                    >
-                        <div className="relative">
-                            <SearchIcon
-                                className="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                            />
-                            <Combobox.Input
-                                className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm"
-                                placeholder="Search..."
-                                onChange={(event) => setQuery(event.target.value)}
-                            />
-                        </div>
+                {filteredTags.length > 0 && (
+                    <Combobox.Options
+                        className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        {filteredTags.map((tag) => (
+                            <Combobox.Option
+                                key={tag.key}
+                                value={tag.value}
+                                className={({active}) =>
+                                    classNames(
+                                        'relative cursor-default select-none py-2 pl-3 pr-9',
+                                        active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                                    )
+                                }
+                            >
+                                {({active, selected}) => (
+                                    <>
+                                        <span
+                                            className={classNames('block truncate', selected && 'font-semibold')}>{tag.key}</span>
 
-                        {filteredPeople.length > 0 && (
-                            <Combobox.Options static
-                                              className="max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800">
-                                {filteredPeople.map((person) => (
-                                    <Combobox.Option
-                                        key={person.id}
-                                        value={person}
-                                        className={({active}) =>
-                                            classNames('cursor-default select-none px-4 py-2', active && 'bg-indigo-600 text-white')
-                                        }
-                                    >
-                                        {person.name}
-                                    </Combobox.Option>
-                                ))}
-                            </Combobox.Options>
-                        )}
-
-                        {query !== '' && filteredPeople.length === 0 && (
-                            <p className="p-4 text-sm text-gray-500">No people found.</p>
-                        )}
-                    </Combobox>
-                </Transition.Child>
-            </Dialog>
-        </Transition.Root>*/
+                                        {selected && (
+                                            <span
+                                                className={classNames(
+                                                    'absolute inset-y-0 right-0 flex items-center pr-4',
+                                                    active ? 'text-white' : 'text-indigo-600'
+                                                )}
+                                            >
+                      </span>
+                                        )}
+                                    </>
+                                )}
+                            </Combobox.Option>
+                        ))}
+                    </Combobox.Options>
+                )}
+            </div>
+        </Combobox>
     )
 }
