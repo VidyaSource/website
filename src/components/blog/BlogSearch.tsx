44145a765da49cd13e33ea83ac1ba8a1eb2c666a
neilchaudhuri <neil@vidyasource.com>
2022-05-04 00:00:29 -0400

    Choices

--- src/components/blog/BlogSearch.tsx
+++ src/components/blog/BlogSearch.tsx
@@ -44,7 +44,7 @@ export default function BlogSearch(p: BlogSearchProps) {
 
                 {filteredTags.length > 0 && (
                     <Combobox.Options
-                        className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-gray-dark ring-opacity-5 focus:outline-none sm:text-md lg:text-2xl">
                         {filteredTags.map((tag) => (
                             <Combobox.Option
                                 key={tag.key}
@@ -52,7 +52,7 @@ export default function BlogSearch(p: BlogSearchProps) {
                                 className={({active}) =>
                                     classNames(
                                         'relative cursor-default select-none py-2 pl-3 pr-9',
-                                        active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                                        active ? 'bg-blue text-white' : 'text-gray-dark'
                                     )
                                 }
                             >
@@ -65,7 +65,7 @@ export default function BlogSearch(p: BlogSearchProps) {
                                             <span
                                                 className={classNames(
                                                     'absolute inset-y-0 right-0 flex items-center pr-4',
-                                                    active ? 'text-white' : 'text-indigo-600'
+                                                    active ? 'text-white' : 'text-gray-dark'
                                                 )}
                                             >
                       </span>
