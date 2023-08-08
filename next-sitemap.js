3c86f7b42299f3a72bed31c2fc898c12f6c48206
neilchaudhuri <neil@vidyasource.com>
2023-08-07 20:30:55 -0400

    Block ChatGPT

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,16 @@
 module.exports = {
     siteUrl: 'https://www.vidyasource.com',
     generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: 'GPTBot',
+                disallow: ['/'],
+            },
+        ],
+    },
 }
