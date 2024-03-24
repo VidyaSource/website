385ee9439a340d35623e62ad60fb93fcc73e56cf
neilchaudhuri <neil@vidyasource.com>
2024-03-24 11:12:33 -0400

    Configure analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import {defineConfig, passthroughImageService} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
@@ -9,32 +9,43 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.vidyasource.com',
-  output: 'hybrid',
-  adapter: cloudflare({
-    routes: {
-      strategy: "include",
-      imageService: "compile"
-    }
-  }),
-  image: {
-    service: passthroughImageService()
-  },
-  integrations: [tailwind(), react(), mdx({
-    markdown: {
-      shikiConfig: {
-        theme: 'dracula',
-        wrap: true
-      }
-    }
-  }), partytown(), AstroPWA(), sitemap()],
-  prefetch: {
-    prefetchAll: true
-  },
-  vite: {
-    ssr: {
-      // Example: Force a broken package to skip SSR processing, if needed
-      external: ['prismjs']
-    }
-  },
+    site: 'https://www.vidyasource.com',
+    output: 'hybrid',
+    adapter: cloudflare({
+        routes: {
+            strategy: "include",
+            imageService: "compile"
+        }
+    }),
+    image: {
+        service: passthroughImageService()
+    },
+    integrations: [
+        tailwind(),
+        react(),
+        mdx({
+            markdown: {
+                shikiConfig: {
+                    theme: 'dracula',
+                    wrap: true
+                }
+            }
+        }),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+        AstroPWA(),
+        sitemap()
+    ],
+    prefetch: {
+        prefetchAll: true
+    },
+    vite: {
+        ssr: {
+            // Example: Force a broken package to skip SSR processing, if needed
+            external: ['prismjs']
+        }
+    },
 });
